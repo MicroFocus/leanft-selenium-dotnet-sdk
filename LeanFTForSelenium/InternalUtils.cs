39996a09f5ee9030225335d61d78412d81282db4
Yevgeny Efter <YevgenyEfter@gmail.com>
2018-03-22 15:26:06 +0200

    Creating a project for the LeanFT for Selenium DotNet SDK, adding some internal implementation, and implementing the ability to highlight web elements and scroll an element into view.

---
+++ LeanFTForSelenium/InternalUtils.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Internal;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace LeanFTForSelenium
+{
+    internal static class InternalUtils
+    {
+        internal static string GetBrowserScript(string scriptName)
+        {
+            string result;
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "LeanFTForSelenium.BrowserScripts." + scriptName;
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                result = reader.ReadToEnd();
+            }
+
+            return result;
+        }
+
+        internal static IJavaScriptExecutor GetExecutor(ISearchContext context)
+        {
+            if (context is IJavaScriptExecutor)
+            {
+                return (IJavaScriptExecutor)context;
+            }
+
+            if (!(context is IWebElement))
+            {
+                throw new InvalidOperationException("Cannot find driver of this element.");
+            }
+
+            IWebDriver driver = ((IWrapsDriver)context).WrappedDriver;
+
+            return (IJavaScriptExecutor)driver;
+        }
+
+        internal static bool IsVisible(IWebElement element)
+        {
+            var elementSize = element.Size;
+            var elementLocation = element.Location;
+
+            var driver = ((IWrapsDriver)element).WrappedDriver;
+            var windowSize = driver.Manage().Window.Size;
+
+            return !((elementSize.Width + elementLocation.X > windowSize.Width) || (elementSize.Height + elementLocation.Y > windowSize.Height));
+        }
+    }
+}
---
+++ LeanFTForSelenium/Utils.cs
@@ -0,0 +1,81 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Threading;
+
+namespace LeanFTForSelenium
+{
+    /// <summary>
+    /// LeanFT for Selenium utilities.
+    /// </summary>
+    public static class Utils
+    {
+        const int ExtraWaitTimeInMiliSec = 500;
+        const int DefaultHighlightTimeInMiliSec = 3000;
+        static string HighlightFunction = InternalUtils.GetBrowserScript("Highlight.js");
+        static string ScrollIntoViewFunction = InternalUtils.GetBrowserScript("ScrollIntoView.js");
+
+        /// <summary>
+        /// Highlights the selenium element in the browser.
+        /// </summary>
+        /// <param name="element">The web element to highlight.</param>
+
+        public static void Highlight(IWebElement element)
+        {
+            Highlight(element, DefaultHighlightTimeInMiliSec);
+        }
+
+        /// <summary>
+        /// Highlights the selenium elements in the browser for time milliseconds.
+        /// </summary>
+        /// <param name="element">The web element to highlight.</param>
+        /// <param name="time">The time (in milliseconds) that the element will be highlighted. In case of a negative number, the method throws an ArgumentException.</param>
+        public static void Highlight(IWebElement element, int time)
+        {
+            if (element == null)
+            {
+                throw new ArgumentNullException("Cannot highlight null object.");
+            }
+
+            if (time < 0)
+            {
+                throw new ArgumentException("The time parameter can't be negative.");
+            }
+
+            if (time == 0)
+            {
+                return;
+            }
+
+            IJavaScriptExecutor executor = InternalUtils.GetExecutor(element);
+
+            // Scroll into the view.
+            ScrollIntoView(element);
+
+            // Highlight the element.
+            executor.ExecuteScript(HighlightFunction, element, time);
+
+            // Wait for the highlight to finish.
+            Thread.Sleep(time + ExtraWaitTimeInMiliSec);
+        }
+
+        /// <summary>
+        /// Scrolls the page to make the web element visible.
+        /// </summary>
+        /// <param name="element">The web element to scroll to.</param>
+        public static void ScrollIntoView(IWebElement element)
+        {
+            if (element == null)
+            {
+                throw new ArgumentNullException("Cannot scroll into view null object.");
+            }
+
+            IJavaScriptExecutor executor = InternalUtils.GetExecutor(element);
+
+            // Scroll into the view.
+            if (!InternalUtils.IsVisible(element))
+            {
+                executor.ExecuteScript(ScrollIntoViewFunction, element);
+            }
+        }
+    }
+}

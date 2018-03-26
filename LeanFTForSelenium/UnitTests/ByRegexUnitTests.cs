321c495fb947800804a4e19ded12757c13a4ea66
Yevgeny Efter <YevgenyEfter@gmail.com>
2018-03-26 09:30:03 +0300

    Adding unit tests for By and ByRegex.

---
+++ LeanFTForSelenium/UnitTests/ByRegexUnitTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+using Moq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace LeanFTForSelenium.UnitTests
+{
+    [TestFixture]
+    class ByRegexUnitTests
+    {
+        [Test]
+        public void FindElements_ContextIsWebElement_ScriptIsExecutedWithElement()
+        {
+            var webElementMock = new Mock<IWebElement>();
+            var javaScriptExecutorMock = webElementMock.As<IJavaScriptExecutor>();
+
+            const string propertyName = "propertyName";
+            const string tagsFilter = "a";
+            var regex = new Regex("Pattern");
+
+            var byRegex = new ByRegex(propertyName, regex, tagsFilter);
+
+            byRegex.FindElements(webElementMock.Object);
+
+            javaScriptExecutorMock.Verify(javaScriptExecutor => javaScriptExecutor.ExecuteScript(It.IsAny<string>(), webElementMock.Object, tagsFilter, propertyName, "Pattern", string.Empty), Times.Once);
+        }
+
+        [Test]
+        public void FindElements_ContextIsNotWebElement_ScriptIsExecutedWithNullAsElement()
+        {
+            var searchContextMock = new Mock<ISearchContext>();
+            var javaScriptExecutorMock = searchContextMock.As<IJavaScriptExecutor>();
+
+            const string propertyName = "propertyName";
+            const string tagsFilter = "a";
+            var regex = new Regex("Pattern");
+
+            var byRegex = new ByRegex(propertyName, regex, tagsFilter);
+
+            byRegex.FindElements(searchContextMock.Object);
+
+            javaScriptExecutorMock.Verify(javaScriptExecutor => javaScriptExecutor.ExecuteScript(It.IsAny<string>(), null, tagsFilter, propertyName, "Pattern", string.Empty), Times.Once);
+        }
+    }
+}

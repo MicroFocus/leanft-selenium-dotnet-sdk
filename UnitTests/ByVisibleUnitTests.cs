ea589fc8343229af24ef661559a10aebe2b00d26
Luiza Mihaela Fat <luizamihaela.fat@microfocus.com>
2023-10-06 16:00:07 +0300

    [Rebranding] Change all appearances of Micro Focus

--- UnitTests/ByVisibleUnitTests.cs
+++ UnitTests/ByVisibleUnitTests.cs
@@ -1,4 +1,4 @@
-﻿/*! (c) Copyright 2015 - 2021 Micro Focus or one of its affiliates. */
+﻿/*! Copyright 2015 - 2021 Open Text. */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // You may not use this file except in compliance with the License.
